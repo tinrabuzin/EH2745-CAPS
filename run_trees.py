2c568e1ed6ca03abaefc42aff253745d43ee7caa
Tin Rabuzin <rabuzin@kth.se>
2017-03-23 10:10:49 +0100

    Adding the latest developments

--- run_trees.py
+++ run_trees.py
@@ -2,7 +2,16 @@ import Trees
 
 x = Trees.decision_tree.BasicTree()
 
-p = x.calc_entropy(3)
-print x.make_tree(x.data, x.classes, x.features)
+data, classes, feature_names =  x.read_data('/Users/tinrabuzin/dev/EH2745-CAPS/Trees/dtree_dataset.csv')
 
-print p
+data = [[10,10], [30,40], [60,70], [80,90],[20,80],[30,70],[70,10],[90,20]]
+classes = [1, 1, 0, 0,1,1,0,0]
+
+
+tree = x.make_tree_cont(data, classes, ['feature1','feature2'])
+
+x.print_tree(tree)
+#p = x.calc_entropy(3)
+#tree = x.make_tree(x.data, x.classes, x.features)
+#x.print_tree(tree)
+#print x.classify(tree, ['Urgent', 'No', 'Yes'], ['Deadline', 'Party', 'Lazy'])
