b8cecc56f5b8fe9b5f43d8d2ee2c6afa53b325f5
Tin Rabuzin <rabuzin@kth.se>
2017-03-27 11:50:42 +0200

    msg3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src'))
+sys.path.insert(0, os.path.abspath('../src'))
+
+print sys.path
 
 # -- General configuration ------------------------------------------------
 
